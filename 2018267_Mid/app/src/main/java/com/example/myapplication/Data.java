aa622ee4cbd0e5e935a981dc8562c2e46ff5f3eb
Hamza Imran <hamzaimran808@gmail.com>
2021-04-20 10:20:59 +0500

    initial commit

---
+++ 2018267_Mid/app/src/main/java/com/example/myapplication/Data.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Data {
+
+    private int userId;
+    private int id;
+    private String title;
+
+    @SerializedName("body")
+    private String name;
+
+    public Data(int userId, int id, String title, String name) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
